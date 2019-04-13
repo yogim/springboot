935894f579b9a75b7545b6780e87dddab298d95a
Yogesh Mhase <yogeshmhase89@gmail.com>
2019-04-13 14:15:28 -0700

    add topic, course

--- src/main/java/com/ymhase/springboot/CourseApiApp.java
+++ src/main/java/com/ymhase/springboot/CourseApiApp.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApiApp {
-    
-    public static void main(String[] args) {
-	SpringApplication.run(CourseApiApp.class, args);
-	
-	
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+
+	}
 
 }
